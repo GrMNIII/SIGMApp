411ae92e2dee122d3ac129212086dc924104939d
GrMNIII <german.morales01@alumnos.ucn.cl>
2025-10-16 06:57:36 -0300

    feat: ip ejemplo client.ts

--- mobile/src/api/client.ts
+++ mobile/src/api/client.ts
@@ -1,7 +1,7 @@
 /*
 import axios from 'axios';
 
-export const BASE_URL = 'http://192.168.1.84:3000/api';
+export const BASE_URL = 'http://192.168.1.XX:3000/api';
 export const api = axios.create({
   baseURL: BASE_URL,
   timeout: 5000,
